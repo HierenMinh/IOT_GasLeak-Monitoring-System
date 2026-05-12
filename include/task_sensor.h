ea3862483f7cd578d2a7a44556fd267179db1243
Huy-pixel <bhminhhuy05@gmail.com>
2026-05-12 18:32:30 +0700

    add detailed information of how to use API

--- include/task_sensor.h
+++ include/task_sensor.h
@@ -17,6 +17,19 @@ typedef struct sensor_t *sensor_handle_t;
 #define COREIOT_BIT (1 << 2)
 #define WEBSERVER_BIT (1 << 3)
 
+/* This API initializes the sensor components and creates the sensor task 
+   Uses: sensor_handle_t sensor;
+         sensor_init(&sensor); */
 void sensor_init(sensor_handle_t *handle);
-/* Every task calls this API must wait for the sensor to be ready by calling sensor_wait_for_warmup, this API will block until the sensor is ready */
+
+/* This API retrieves data from the sensor 
+   Uses: sensor_data_t data;
+         sensor_get_data(sensor, &data, UI_BIT, portMAX_DELAY); 
+@params handle: The sensor handle
+        data: Pointer to the sensor data structure that receives the data
+        myBits: The event bits to wait for, each bit corresponds to a task that needs the data. 
+                If no new data is available, means that sensor have not been updated since the last time the task retrieved data, 
+                or task reads faster than the sensor update rate, this API returns false.
+        timeout: The timeout value, in ticks, to wait for data to become available. Use portMAX_DELAY to wait indefinitely.
+@returns: true if data is available, false otherwise */
 bool sensor_get_data(sensor_handle_t handle, sensor_data_t *data, EventBits_t myBits, TickType_t timeout);
